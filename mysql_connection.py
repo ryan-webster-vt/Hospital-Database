83bd7fc196f214e34bee2e2b03f62bf100daf9bc
Junsoo Kim <155798033+jooncybertest@users.noreply.github.com>
2025-04-01 20:09:05 -0400

    completed database setup locally

--- mysql_connection.py
+++ mysql_connection.py
@@ -7,7 +7,7 @@ load_dotenv()
 config = {
     'host': os.getenv('DB_HOST'),
     'user': os.getenv('DB_USER'),
-    'password': os.getenv('DB_PASSWORD'),
+    'password': os.getenv('DB_PASSWORD', ''),
     'database': os.getenv('DB_NAME')
 }
 
